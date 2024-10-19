d1f7d695fa71ccb3cac2e47bc0ce35744947af26
jgaffuri <jgaf@proton.me>
2024-10-19 19:31:11 +0200

    a

--- src/process.py
+++ src/process.py
@@ -3,7 +3,11 @@ from pygridmap import gridtiler
 from datetime import datetime
 import os
 
-transform = True
+
+tmpfolder = "/home/juju/geodata/census/tmp/"
+
+transform = False
+aggregate = True
 
 
 #GRD_ID,T,M,F,Y_LT15,Y_1564,Y_GE65,EMP,NAT,EU_OTH,OTH,SAME,CHG_IN,CHG_OUT,LAND_SURFACE,POPULATED,CONFIDENTIALSTATUS
@@ -21,9 +25,19 @@ if transform:
         c['y'] = gid[0]
         if c['CONFIDENTIALSTATUS'] == "": c['CONFIDENTIALSTATUS'] = 0
         return c
-    gridtiler.grid_transformation("/home/juju/geodata/census/2021/ESTAT_Census_2021_V2.csv", tr, "/home/juju/geodata/census/tmp/1000.csv")
+    gridtiler.grid_transformation("/home/juju/geodata/census/2021/ESTAT_Census_2021_V2.csv", tr, tmpfolder+"1000.csv")
 
 
+
+#aggregation
+if aggregate:
+
+    for a in [2,5,10]:
+        print(datetime.now(), "aggregation to", a*1000, "m")
+        gridtiler.grid_aggregation(input_file=tmpfolder+"1000.csv", resolution=1000, output_file=tmpfolder+str(a*1000)+".csv", a=a)
+    for a in [2,5,10]:
+        print(datetime.now(), "aggregation to", a*10000, "m")
+        gridtiler.grid_aggregation(input_file=tmpfolder+"10000.csv", resolution=10000, output_file=tmpfolder+str(a*10000)+".csv", a=a)
 
 
 
@@ -32,45 +46,18 @@ if transform:
 exit()
 
 format_join = False
-aggregate = False
 tiling = False
 
 folder = "/home/juju/geodata/elections_fr/leg2024/"
 
 
-#aggregation
-if aggregate:
 
-    #codeDepartement	nomDepartement	codeCirconscription	nomCirconscription	codeCommune	nomCommune	codeBureauVote
-    aggregation_fun = {
-        "codeDepartement": gridtiler.aggregation_single_value,
-        "nomDepartement": gridtiler.aggregation_single_value,
-        "codeCirconscription": gridtiler.aggregation_single_value,
-        "nomCirconscription": gridtiler.aggregation_single_value,
-        "codeCommune": gridtiler.aggregation_single_value,
-        "nomCommune": gridtiler.aggregation_single_value,
-        #TODO "codeBureauVote": gridtiler.aggregation_single_value,
-        }
-
-    print(datetime.now(), "aggregation to", 100, "m")
-    gridtiler.grid_aggregation(input_file=folder + "1.csv", resolution=1, output_file=folder+"100.csv", a=100, aggregation_fun=aggregation_fun)
-
-    for a in [2,5,10]:
-        print(datetime.now(), "aggregation to", a*100, "m")
-        gridtiler.grid_aggregation(input_file=folder+"100.csv", resolution=100, output_file=folder+str(a*100)+".csv", a=a, aggregation_fun=aggregation_fun)
-    for a in [2,5,10]:
-        print(datetime.now(), "aggregation to", a*1000, "m")
-        gridtiler.grid_aggregation(input_file=folder+"1000.csv", resolution=1000, output_file=folder+str(a*1000)+".csv", a=a, aggregation_fun=aggregation_fun)
-    for a in [2,5,10]:
-        print(datetime.now(), "aggregation to", a*10000, "m")
-        gridtiler.grid_aggregation(input_file=folder+"10000.csv", resolution=1000, output_file=folder+str(a*10000)+".csv", a=a, aggregation_fun=aggregation_fun)
-
-    print(datetime.now(), "clean")
-    for resolution in [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]:
-        f = folder+str(resolution)+".csv"
-        df = pd.read_csv(f)
-        df.loc[df['nb_bv'] > 1, ['codeDepartement', 'nomDepartement', 'codeCirconscription', 'codeCommune', 'nomCirconscription', 'nomCommune', 'codeBureauVote']] = None
-        df.to_csv(f, index=False)
+print(datetime.now(), "clean")
+for resolution in [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]:
+    f = folder+str(resolution)+".csv"
+    df = pd.read_csv(f)
+    df.loc[df['nb_bv'] > 1, ['codeDepartement', 'nomDepartement', 'codeCirconscription', 'codeCommune', 'nomCirconscription', 'nomCommune', 'codeBureauVote']] = None
+    df.to_csv(f, index=False)
 
 
 
