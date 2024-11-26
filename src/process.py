2417d1d47e0d6dcc8ca91865143e54a2eae98264
jgaffuri <jgaf@proton.me>
2024-11-26 11:26:39 +0100

    a

--- src/process.py
+++ src/process.py
@@ -9,7 +9,6 @@ aggregated_folder = "/home/juju/geodata/census/2021/aggregated/"
 transform = True
 aggregate = True
 tiling = True
-make_csv = False
 
 #GRD_ID,T,M,F,Y_LT15,Y_1564,Y_GE65,EMP,NAT,EU_OTH,OTH,SAME,CHG_IN,CHG_OUT,LAND_SURFACE,POPULATED,CONFIDENTIALSTATUS
 #transform
@@ -17,10 +16,10 @@ make_csv = False
 if transform:
     def tr(c):
 
-        #skip non populated cells
+        # skip non populated cells
         if c['T'] == "0": return False
 
-        #remove useless information
+        # remove useless information
         del c['LAND_SURFACE']
         del c['POPULATED']
 
@@ -30,8 +29,8 @@ if transform:
         c['x'] = gid[1]
         c['y'] = gid[0]
 
-        #fix: mark data as not available
-        #not available data is marked as ""
+        # fix: mark data as not available
+        # not available data is marked as ""
         if( c['M'] == "0" and c['F'] == "0" ):
             c['M'] = ""
             c['F'] = ""
@@ -48,7 +47,7 @@ if transform:
             c['CHG_IN'] = ""
             c['CHG_OUT'] = ""
 
-        #ensures confidentialstatus is 0 or 1
+        # ensures confidentialstatus is 0 or 1
         if c['CONFIDENTIALSTATUS'] == "": c['CONFIDENTIALSTATUS'] = 0
 
     gridtiler.grid_transformation("/home/juju/geodata/census/2021/ESTAT_Census_2021_V2.csv", tr, aggregated_folder+"1000.csv")
@@ -58,7 +57,8 @@ if transform:
 #aggregation
 if aggregate:
 
-    #the aggregation function:
+    # the aggregation function:
+    # as soon as one value is NA, then the aggregated is NA
     def aggregation_sum_NA(values, _=0):
         sum = 0
         for value in values:
@@ -68,7 +68,7 @@ if aggregate:
     aggregation_fun = {}
     for code in ["M","F","Y_LT15","Y_1564","Y_GE65","EMP","NAT","EU_OTH","OTH","SAME","CHG_IN","CHG_OUT"]: aggregation_fun[code] = aggregation_sum_NA
 
-
+    # launch aggregations
     for a in [2,5,10]:
         print(datetime.now(), "aggregation to", a*1000, "m")
         gridtiler.grid_aggregation(input_file=aggregated_folder+"1000.csv", resolution=1000, output_file=aggregated_folder+str(a*1000)+".csv", a=a, aggregation_fun=aggregation_fun)
@@ -79,7 +79,7 @@ if aggregate:
 
 
 
-#tiling
+# tiling
 if tiling:
     for resolution in [1000, 2000, 5000, 10000, 20000, 50000, 100000]:
         print(datetime.now(), "tiling for resolution", resolution)
@@ -88,6 +88,7 @@ if tiling:
         out_folder = 'pub/v2/parquet/' + str(resolution)
         if not os.path.exists(out_folder): os.makedirs(out_folder)
 
+        # launch tiling
         gridtiler.grid_tiling(
             aggregated_folder+str(resolution)+".csv",
             out_folder,
@@ -97,17 +98,3 @@ if tiling:
             y_origin = 0,
             format = "parquet"
         )
-
-        if make_csv:
-
-            out_folder = 'pub/v2/csv/' + str(resolution)
-            if not os.path.exists(out_folder): os.makedirs(out_folder)
-            gridtiler.grid_tiling(
-                aggregated_folder+str(resolution)+".csv",
-                out_folder,
-                resolution,
-                tile_size_cell = 256,
-                x_origin = 0,
-                y_origin = 0,
-                format = "csv"
-            )
