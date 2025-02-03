f5f09204a77884837e1bb4d6c899ca1f12cfaa6e
jgaffuri <jgaf@proton.me>
2025-02-03 16:56:22 +0100

    a

--- src/process.py
+++ src/process.py
@@ -6,8 +6,8 @@ import os
 aggregated_folder = "/home/juju/geodata/census/2021/aggregated/"
 
 
-transform = False
-aggregate = False
+transform = True
+aggregate = True
 tiling = True
 
 # fid,GRD_ID,T,M,F,Y_LT15,Y_1564,Y_GE65,EMP,NAT,EU_OTH,OTH,SAME,CHG_IN,CHG_OUT,LAND_SURFACE,POPULATED,COUNT,
